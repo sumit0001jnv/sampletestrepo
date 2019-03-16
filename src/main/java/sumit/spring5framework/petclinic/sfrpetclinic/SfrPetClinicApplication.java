9c28798bc0cfd2f007866766945af525f8cb6c43
sumit0001jnv <sumit0001.jnv@gmail.com>
2019-03-16 19:12:06 +0530

    First commit

---
+++ src/main/java/sumit/spring5framework/petclinic/sfrpetclinic/SfrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package sumit.spring5framework.petclinic.sfrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfrPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfrPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sumit/spring5framework/petclinic/sfrpetclinic/SfrPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package sumit.spring5framework.petclinic.sfrpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfrPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
